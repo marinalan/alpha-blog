4e3bd5c987212c1e2afc408538777e42d9c0d35f
Marina Landisberg <mlandisberg@gmail.com>
2021-01-29 19:07:19 -0800

    styling of home and articles listing pages

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,16 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require("webpack");
 
-const webpack = require("webpack")
-
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-}));
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    jquery: "jquery",
+    "window.Tether": "tether",
+    Popper: ["popper.js", "default"] // for Bootstrap 4
+  })
+);
 
 module.exports = environment
